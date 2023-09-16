30d29c1747fc702b3e5e5414e40294e8d3789e64
JuanDiegoSR28 <juan.sanchez233013@potros.itson.edu.mx>
2023-09-15 19:40:27 -0700

    Primera instancia del proyecto

---
+++ src/main/java/com/mycompany/proyectoparcial01_equipo04/Proyectoparcial01_Equipo04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoparcial01_equipo04;
+
+/**
+ *
+ * @author PC
+ */
+public class Proyectoparcial01_Equipo04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
